536dbb9bd7986944eec479604a7daa57b351f47b
chilyjunior0-maker <chilyjunior0@gmail.com>
2026-08-18 13:23:54 +0000

    Update CACHE_NAME to version 4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'quiz-islam-arabe-v3';
+const CACHE_NAME = 'quiz-islam-arabe-v4';
 
 // Liste des fichiers essentiels à mettre en cache pour le fonctionnement hors ligne
 const ASSETS_TO_CACHE = [
